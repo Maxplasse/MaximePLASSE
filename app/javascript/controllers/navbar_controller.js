8700ebec2b31cc3c5e448ba5d9a01a97227fad2d
Maxime Plasse <m20plasse@gmail.com>
2023-08-31 11:27:22 +0200

    carousel responsive

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -39,7 +39,7 @@ export default class extends Controller {
     const scrollAmount = window.scrollY;
     const windowHeight = window.innerHeight;
 
-    // Define an array of objects for different device widths and scroll positions
+
     const deviceConfig = [
       { maxWidth: 375, scrollPosition: 435, smallHeightScrollPosition: 400 },
       { maxWidth: 390, scrollPosition: 435, smallHeightScrollPosition: 400 },
@@ -48,10 +48,10 @@ export default class extends Controller {
       { maxWidth: 992, scrollPosition: 360, smallHeightScrollPosition: 330 },
       { maxWidth: 1200, scrollPosition: 390, smallHeightScrollPosition: 350 },
       { maxWidth: 2600, scrollPosition: 390, smallHeightScrollPosition: 350 },
-      // Add more as needed
+
     ];
 
-    // Find the appropriate device configuration
+
     const currentConfig = deviceConfig.find(config => window.matchMedia(`(max-width: ${config.maxWidth}px)`).matches);
 
     let scrollPositionThreshold = currentConfig.scrollPosition;
@@ -61,7 +61,7 @@ export default class extends Controller {
     }
 
     if (currentConfig) {
-      // Calculate the scroll position to trigger transparency (half of viewport height)
+
       const transparentScrollPosition = windowHeight * 0.8;
 
       if (scrollAmount > transparentScrollPosition) {
@@ -86,15 +86,15 @@ export default class extends Controller {
     const logoElement = this.logoTarget;
     const languageElement = this.languageTarget;
 
-    // Adjust these values as needed
-    const startScroll = 250; // The scroll position where the effect starts for text
-    const startScrollLogo = 250; // The scroll position where the effect starts for logo
-    const startScrollLanguage = 250; // The scroll position where the effect starts for language
+
+    const startScroll = 250;
+    const startScrollLogo = 250;
+    const startScrollLanguage = 250;
     const endScroll = 600;
 
     const windowWidth = window.innerWidth;
 
-    let moveFactor = 0.8; // Default moveFactor for larger screens
+    let moveFactor = 0.8;
 
     if (windowWidth <= 390) {
       moveFactor = 0.3;
@@ -120,20 +120,19 @@ export default class extends Controller {
         logoElement.style.transform = `translateX(${logoTranslate}px)`;
         languageElement.style.transform = `translateX(${languageTranslate}px)`;
 
-        // Check if the language element is moving to the right and apply overflow hidden
         if (languageTranslate > 0) {
           document.body.style.overflowX = "hidden";
         } else {
           document.body.style.overflowX = "auto";
         }
       }
-    } else {
-      textElement.style.opacity = 1;
-      logoElement.style.transform = "translateX(0)";
-      languageElement.style.transform = "translateX(0)";
-      languageElement.style.position = "static";
-      document.body.style.overflowX = "auto";
-    }
+      } else {
+        textElement.style.opacity = 1;
+        logoElement.style.transform = "translateX(0)";
+        languageElement.style.transform = "translateX(0)";
+        languageElement.style.position = "static";
+        document.body.style.overflowX = "auto";
+      }
   }
 
 
