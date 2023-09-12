51dcc469454bfa382836c08bf1f27d7fd6621ba3
Maxime Plasse <m20plasse@gmail.com>
2023-09-12 08:58:58 +0200

    adding new cv

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,6 +1,6 @@
 class ResumesController < ApplicationController
   def download
-    resume_path = Rails.root.join('public', 'resumes', 'resume.pdf')
-    send_file resume_path, filename: 'resume.pdf', type: 'application/pdf'
+    resume_path = Rails.root.join('public', 'resumes', 'Resume Maxime Plasse.pdf')
+    send_file resume_path, filename: 'Resume Maxime Plasse.pdf', type: 'application/pdf'
   end
 end
