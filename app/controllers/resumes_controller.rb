82c58197608c083dbd31ab3621c623d1ed487e38
Maxime Plasse <m20plasse@gmail.com>
2023-11-14 13:14:55 +0100

    done

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,6 +1,6 @@
 class ResumesController < ApplicationController
   def download
-    resume_path = Rails.root.join('public', 'resumes', 'Resume Maxime Plasse.pdf')
-    send_file resume_path, filename: 'Resume Maxime Plasse.pdf', type: 'application/pdf'
+    resume_path = Rails.root.join('public', 'resumes', 'CV_MAXIME_PLASSE.pdf')
+    send_file resume_path, filename: 'CV_MAXIME_PLASSE.pdf', type: 'application/pdf'
   end
 end
